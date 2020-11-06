39efb86ab0b847f03146cc0a53e93a2aea883b52
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-11-06 20:49:57 +0100

    update deps

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
-// TASK 1 - import the axios lib from node_modules
+// 👉 TASK 1 - import the axios lib from node_modules
 
-// TASK 2 - import the contants from constants/index.js
+// 👉 TASK 2 - import the contants from constants/index.js
 
 import Details from './Details'
 
@@ -17,7 +17,7 @@ export default function App() {
     setCurrentFriendId(null)
   }
 
-  // TASK 3 - make an effect that runs after FIRST DOM surgery
+  // 👉 TASK 3 - make an effect that runs after FIRST DOM surgery
   // caused by the first render only. You'll need `useEffect` from React.
   // The effect should consist of a call to the API using axios.
   // On success, set the array of friend objects from the API into state.
--- src/components/Details.js
+++ src/components/Details.js
@@ -6,15 +6,15 @@ export default function Details(props) {
   const { friendId, close } = props
   const [details, setDetails] = useState(null)
 
-  // TASK 4 - Create a side effect 🥇 that runs only after first render.
+  // 👉 TASK 4 - Create a side effect 🥇 that runs only after first render.
 
-  // TASK 5 - Create a side effect 👻 that runs only after first render
+  // 👉 TASK 5 - Create a side effect 👻 that runs only after first render
   // and puts a 'click' event handler on document.
   // See what happens if we don't clean up.
 
-  // TASK 6 - Create a side effect 🥵 that runs after every render.
+  // 👉 TASK 6 - Create a side effect 🥵 that runs after every render.
 
-  // TASK 7 - Create a side effect 📲 that runs when a particular variable changes:
+  // 👉 TASK 7 - Create a side effect 📲 that runs when a particular variable changes:
   // Whenever props.friendId updates we should trigger a fetch for details of the friend.
   // The URL should end up looking like `http://localhost:4000/friends/1?api_key=xyz`
   // On success, shove the details of the friend in `details` slice of state
@@ -29,9 +29,7 @@ export default function Details(props) {
           <p>email is {details.email}</p>
           {name} likes:
           <ul>
-            {
-              details.hobbies.map((hobby) => <li key={hobby}>{hobby}</li>)
-            }
+            {details.hobbies.map((hobby) => <li key={hobby}>{hobby}</li>)}
           </ul>
         </>
       }
